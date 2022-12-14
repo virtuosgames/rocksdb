059116d4845766e5f1fea0b6d7b3baafcc6e9643
renjianzhong <renjianzhong2966@virtuos.com.cn>
2022-12-14 09:52:35 +0800

    [VTS] Modify the shell script

--- build-android.sh
+++ build-android.sh
@@ -1,16 +1,18 @@
-export NDK=E:/android-ndk-r21d
-export STRIP=E:/android-ndk-r21d/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64/bin
-export toolchain=E:/android-ndk-r21d/build/cmake/android.toolchain.cmake
-gernerator="Ninja"
+export STRIP="E:/android-ndk-r21d/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64/bin/"
+#export BUILD_TOOL_PATH="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE/CommonExtensions/Microsoft/CMake/Ninja/"
+NDK="E:/android-ndk-r21d"
+TOOLCHAIN="E:/android-ndk-r21d/build/cmake/android.toolchain.cmake"
+GENERATOR="Ninja"
 
 function build(){
     ABI=$1
     BUILD_PATH=build.android.${ABI}
-    cmake -S . -B ${BUILD_PATH} -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=${toolchains} -DANDROID_NDK=${NDK} -DANDROID_ABI=${ABI} -DCMAKE_GENERATOR=${gernerator} -DWITH_GFLAGS=0 -DWITH_SNAPPY=0 -DWITH_JEMALLOC=0 -DWITH_JNI=0 -DWITH_BENCHMARK_TOOLS=0 -DWITH_CORE_TOOLS=0 -DWITH_TOOLS=0
+	OUT_PATH="./Plugins/Android/libs/${ABI}"
+    cmake -S . -B ${BUILD_PATH} -DANDROID=1 -DCMAKE_MAKE_PROGRAM="ninja" -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN} -DANDROID_NDK=${NDK} -DANDROID_ABI=${ABI} -DCMAKE_GENERATOR=${GENERATOR} -DWITH_GFLAGS=0 -DWITH_SNAPPY=0 -DWITH_JEMALLOC=0 -DWITH_JNI=0 -DWITH_BENCHMARK_TOOLS=0 -DWITH_CORE_TOOLS=0 -DWITH_TOOLS=0
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p Plugins/Android/libs/${ABI}/
-    cp ${BUILD_PATH}/librocksdb.so Plugins/Android/libs/${ABI}/librocksdb.so
+    cp ${BUILD_PATH}/librocksdb.so ${OUT_PATH}/librocksdb.so
 }
 
-build armeabi-v7a 
+#build armeabi-v7a
 build arm64-v8a
